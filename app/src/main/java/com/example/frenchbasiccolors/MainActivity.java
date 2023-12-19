42f71c3340fc2029d3c31dd80299730d70f0be8d
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-19 03:27:25 -0500

    mp3 files added

--- app/src/main/java/com/example/frenchbasiccolors/MainActivity.java
+++ app/src/main/java/com/example/frenchbasiccolors/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.frenchbasiccolors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button blackButton = findViewById(R.id.blackButt);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaPlayer blackFr = MediaPlayer.create(this,R.raw)
     }
 }
